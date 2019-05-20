fcfe7f7d10620845c0eafc2aa086c9015d7359e8
OnionTraveler <47844836+OnionTraveler@users.noreply.github.com>
2019-05-21 01:21:53 +0800

    Update identify.py

--- identify.py
+++ identify.py
@@ -28,8 +28,7 @@ def identify(modulePath,predictPicturePath):
     return onion.predict(xs).argmax(axis=-1), types[onion.predict(xs).argmax(axis=-1)[0]]
 
 if __name__=='__main__':
-    result=identify('./modules/XXX.h5',"E:\Brain_suit.jpg")
+    result=identify('./modules/XXX.h5',"XXX.jpg")
     print("Identification Result INDEX:", result[0])
     print("Identification Result:", result[1])
-    
-    
+       
