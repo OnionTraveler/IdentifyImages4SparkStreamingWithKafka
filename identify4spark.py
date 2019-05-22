4cd161dc21d41c25c72402233ad263ff694cbaaf
OnionTraveler <47844836+OnionTraveler@users.noreply.github.com>
2019-05-22 19:38:30 +0800

    Update identify4spark.py

--- identify4spark.py
+++ identify4spark.py
@@ -3,7 +3,6 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
-
 def identify(predictPicture):
     # list all types what the model identified
     # REMEMBER LABELS in your model have to be defined as following in this .py
@@ -15,7 +14,7 @@ def identify(predictPicture):
     onion = load_model("clothes5_4310.h5")  # "./modules/clothes5_4310.h5"
 
     import time
-    time.sleep(3)
+    time.sleep(1)
 
     import base64
     data=base64.b64decode(predictPicture)
@@ -27,7 +26,6 @@ def identify(predictPicture):
 
     # 再用的時候所有東西都轉成np array
     from keras.applications.vgg16 import preprocess_input
-    time.sleep(3)
     imglist = []
     imglist.append(preprocess_input(np.array(fn)))
     xs = np.array(imglist)
@@ -35,7 +33,6 @@ def identify(predictPicture):
     time.sleep(1)  # 休息1秒
     # Identification Result
     return types[onion.predict(xs).argmax(axis=-1)[0]]
-
 
 if __name__ == "__main__":
     sc = SparkContext()
@@ -51,3 +48,6 @@ if __name__ == "__main__":
     ssc.start()
     ssc.awaitTermination()
 
+
+#========================= ( CMD 4 running this file(identify4spark.py) in Spark Streaming )
+# spark-submit --master spark://172.21.0.2:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.1 --files modules/clothes5_4310.h5 identify4spark.py
